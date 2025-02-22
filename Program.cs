f2855aef095ef1fc99c3b1ed84a5d8910bcdfb62
zDany01 <85403430+zDany01@users.noreply.github.com>
2025-02-22 14:01:17 +0100

    Initial App Structure

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace win_audio_analyzer
+{
+    static class Program
+    {
+        public const string APP_NAME = "win-audio-analyzer";
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GUI());
+        }
+    }
+}
